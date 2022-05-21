0e641a059572052a0f6aea877e4920b7c613bdc2
Nate Catelli <ncatelli@packetfire.org>
2022-05-21 13:21:26 -0500

    Subcommands for site generation (#3)
    * breakup into subcommands
    
    * add all subcommand
    
    * remove extra whitespace
    
    * add hugo to devcontainer
    
    * stub site
    
    * WIP: Site generation
    
    * mount site
    
    * update templates
    
    * First pass site
    
    * update demo tag
    
    * new help printer
    
    * ignore demo content fom git

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"path/filepath"
 	"strings"
 	"text/template"
+	"time"
 
 	"github.com/SlyMarbo/rss"
 )
@@ -26,12 +27,27 @@ const (
 {{ .Link }}
 ----
 `
+
+	defaultGeneratedSiteFormatting string = `---
+title: {{ .Meta.Title  }}
+date: {{ .Meta.Date  }}
+cve: {{ .Meta.Link  }}
+tags: {{ range .Meta.Tags }}
+  - {{. | js}}{{end}}
+draft: false
+---
+
+<a href="{{ .Meta.Link }}">{{ .Meta.Link }}</a>
+	
+{{ .Summary }}
+`
 )
 
 var (
 	feedUrl      string
 	confPath     string
 	cachePath    string
+	sitePath     string
 	formatOutput string
 )
 
@@ -78,68 +94,67 @@ func cacheFeed(cachePath string, feed *rss.Feed) error {
 	return nil
 }
 
-func printHelp() {
-	fmt.Println("Usage: sec-feed [OPTIONS]...")
-	fmt.Printf("A cli checker utility for generating vulnerabilty feeds.\n\n")
-	flag.PrintDefaults()
-}
-
-func main() {
-	help := flag.Bool("help", false, "print help information")
-	flag.StringVar(&feedUrl, "url", getEnvOr("SEC_FEED_URL", defaultRssFeedSource), "the url source feed")
-	flag.StringVar(&confPath, "filter-path", getEnvOr("SEC_FEED_FILTER_PATH", "conf"), "the directory path to source filters from")
-	flag.StringVar(&cachePath, "cache-path", getEnvOr("SEC_FEED_CACHE_PATH", ".sec-feed/"), "the directory path to store all cache files")
-	flag.StringVar(&formatOutput, "format", getEnvOr("SEC_FEED_OUTPUT_FORMAT", defaultOutputFormatting), "a formatting string for the resulting output data")
-	flag.Parse()
-
-	if *help {
-		printHelp()
-		os.Exit(0)
-	}
-
-	filters, err := WalkAllFilesInFilterDir(filepath.Clean(confPath))
-	if err != nil {
-		log.Fatal("failed to vulnerability filters.")
-	}
-
+func fetch_feed(feedUrl, absoluteCacheFilePath string, ignoreUpdate bool) (*rss.Feed, bool, error) {
 	req, err := url.Parse(feedUrl)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var newItems []*rss.Item
-
-	absoluteCacheFilePath := filepath.Join(cachePath, cacheFile)
-
 	feed, err := loadCachedFeed(absoluteCacheFilePath)
+	cached := false
+
+	// update the feed from cache
 	if !errors.Is(err, os.ErrNotExist) {
 		err := feed.Update()
-		if err != nil {
-			log.Fatal(err)
+		if err != nil && feed != nil && ignoreUpdate {
+			return feed, true, nil
+		} else if err != nil {
+			return nil, cached, err
 		}
+
+		cached = true
+	} else {
+		upstream, err := rss.Fetch(req.String())
+		if err != nil {
+			return nil, cached, err
+		}
+
+		feed = upstream
+		cached = false
+	}
+
+	return feed, cached, nil
+}
+
+func printHelp() {
+	fmt.Println("Usage: sec-feed [OPTIONS]...")
+	fmt.Printf("A cli checker utility for generating vulnerabilty feeds.\n")
+	fmt.Printf("commands:\n")
+	fmt.Printf("  new\n  all\n  generate\n")
+	fmt.Printf("flags:\n")
+
+	flag.PrintDefaults()
+}
+
+func cmdNewItems(feed *rss.Feed, cacheFilePath string, filters map[string]string, cached bool) error {
+	var newItems []*rss.Item
+
+	if cached {
 		for _, item := range feed.Items {
 			if !item.Read {
 				newItems = append(newItems, item)
 			}
 		}
-	} else {
-		upstream, err := rss.Fetch(req.String())
-		if err != nil {
-			log.Fatalf("invalid upstream url: %s\n", req.String())
-		}
-
-		feed = upstream
-		// no new items are append on a new url.
 	}
 
-	if err = cacheFeed(absoluteCacheFilePath, feed); err != nil {
-		log.Fatalf("failed to cache %s: %s\n", absoluteCacheFilePath, err)
+	if err := cacheFeed(cacheFilePath, feed); err != nil {
+		return fmt.Errorf("failed to cache %s: %s", cacheFilePath, err)
 	}
 
 	// setup template
 	outputTemplate, err := template.New("output").Parse(formatOutput)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	var newItemsMatchingFilters []*rss.Item
@@ -155,8 +170,171 @@ func main() {
 	for _, item := range newItemsMatchingFilters {
 		err = outputTemplate.Execute(os.Stdout, item)
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
 	}
 
+	return nil
+}
+
+func cmdAll(feed *rss.Feed, cacheFilePath string, filters map[string]string) error {
+	if err := cacheFeed(cacheFilePath, feed); err != nil {
+		return fmt.Errorf("failed to cache %s: %s", cacheFilePath, err)
+	}
+
+	// setup template
+	outputTemplate, err := template.New("output").Parse(formatOutput)
+	if err != nil {
+		return err
+	}
+
+	var itemsMatchingFilters []*rss.Item
+	for _, item := range feed.Items {
+		for _, filter := range filters {
+			if strings.Contains(item.Title, filter) {
+				itemsMatchingFilters = append(itemsMatchingFilters, item)
+				break
+			}
+		}
+	}
+
+	for _, item := range itemsMatchingFilters {
+		err = outputTemplate.Execute(os.Stdout, item)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Item represents a single story.
+type PageMeta struct {
+	Title string    `json:"title" yaml:"title"`
+	Link  string    `json:"link" yaml:"link"`
+	Date  time.Time `json:"date" yaml:"date"`
+	Tags  []string  `json:"tags" yaml:"tags"`
+}
+
+type PageData struct {
+	Meta    PageMeta `json:"title" yaml:"meta"`
+	Summary string   `json:"summary" yaml:"summary"`
+}
+
+func cmdGenerate(feed *rss.Feed, cacheFilePath string, siteFilePath string, filters map[string]string) error {
+	if err := cacheFeed(cacheFilePath, feed); err != nil {
+		return fmt.Errorf("failed to cache %s: %s", cacheFilePath, err)
+	}
+
+	// setup template
+	outputTemplate, err := template.New("hugo").Parse(defaultGeneratedSiteFormatting)
+	if err != nil {
+		return err
+	}
+
+	var itemsMatchingFilters []*rss.Item
+	for _, item := range feed.Items {
+		for _, filter := range filters {
+			if strings.Contains(item.Title, filter) {
+				itemsMatchingFilters = append(itemsMatchingFilters, item)
+				break
+			}
+		}
+	}
+
+	for _, item := range itemsMatchingFilters {
+		tmp := strings.Split(item.Title, "(")
+		tmpTags := strings.Trim(tmp[1], "()")
+		tmpTags = strings.TrimSpace(tmpTags)
+		tags := strings.Split(tmpTags, ", ")
+
+		title := strings.TrimSpace(tmp[0])
+
+		meta := PageMeta{
+			Title: title,
+			Link:  item.Link,
+			Date:  item.Date,
+			Tags:  tags,
+		}
+
+		data := PageData{
+			Meta:    meta,
+			Summary: item.Summary,
+		}
+
+		lowerCve := filepath.Clean(strings.ToLower(meta.Title))
+		fileName := filepath.Join(siteFilePath, "/content/cve/", (lowerCve + ".md"))
+		f, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			return err
+		}
+
+		err = outputTemplate.Execute(f, data)
+		f.Close()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	help := flag.Bool("help", false, "print help information")
+	flag.StringVar(&feedUrl, "url", getEnvOr("SEC_FEED_URL", defaultRssFeedSource), "the url source feed")
+	flag.StringVar(&confPath, "filter-path", getEnvOr("SEC_FEED_FILTER_PATH", "conf"), "the directory path to source filters from")
+	flag.StringVar(&cachePath, "cache-path", getEnvOr("SEC_FEED_CACHE_PATH", ".sec-feed"), "the directory path to store all cache files")
+	flag.StringVar(&sitePath, "site-path", getEnvOr("SEC_FEED_SITE_PATH", "site"), "the directory path to the hugo root.")
+	flag.StringVar(&formatOutput, "format", getEnvOr("SEC_FEED_OUTPUT_FORMAT", defaultOutputFormatting), "a formatting string for the resulting output data")
+	flag.Parse()
+
+	if *help {
+		printHelp()
+		os.Exit(0)
+	}
+
+	absoluteCacheFilePath := filepath.Join(cachePath, cacheFile)
+	filters, err := WalkAllFilesInFilterDir(filepath.Clean(confPath))
+	if err != nil {
+		log.Fatal("failed to vulnerability filters.")
+	}
+
+	cmd := flag.Arg(0)
+	switch cmd {
+	case "new":
+		feed, cached, err := fetch_feed(feedUrl, absoluteCacheFilePath, false)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = cmdNewItems(feed, absoluteCacheFilePath, filters, cached)
+		if err != nil {
+			log.Fatal(err)
+		}
+	case "all":
+		feed, _, err := fetch_feed(feedUrl, absoluteCacheFilePath, true)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = cmdAll(feed, absoluteCacheFilePath, filters)
+		if err != nil {
+			log.Fatal(err)
+		}
+	case "generate":
+		feed, _, err := fetch_feed(feedUrl, absoluteCacheFilePath, true)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = cmdGenerate(feed, absoluteCacheFilePath, filepath.Clean(sitePath), filters)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	case "":
+		log.Fatal("command not specified")
+	default:
+		log.Fatalf("invalid command: %s", cmd)
+	}
 }
